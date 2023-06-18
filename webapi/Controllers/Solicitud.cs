a5523e18e8a45c45faa2229a7369cb55eaf73868
Marcos Coca <marcoscoca0905@gmail.com>
2023-06-18 15:10:47 -0400

    complete

---
+++ webapi/Controllers/Solicitud.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using webapi.DB;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Solicitud : ControllerBase
+    {
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Ok>> PostSolicitud(Solicitude newSolicitud)
+        {
+
+            using var db = new VisaContext();
+            var solicitud = new Solicitude
+            {
+                Apellidos = newSolicitud.Apellidos,
+                Direccion = newSolicitud.Direccion,
+                Nombres = newSolicitud.Nombres,
+                Sexo = newSolicitud.Sexo,
+                Telefono = newSolicitud.Telefono,
+                Foto = newSolicitud.Foto
+            };
+            db.Solicitudes.Add(solicitud);
+            await db.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ webapi/DB/Solicitude.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace webapi.DB;
+
+public partial class Solicitude
+{
+    public int Id { get; set; }
+
+    public string Nombres { get; set; } = null!;
+
+    public string Apellidos { get; set; } = null!;
+
+    public string Sexo { get; set; } = null!;
+
+    public string Telefono { get; set; } = null!;
+
+    public string Direccion { get; set; } = null!;
+
+    public string Foto { get; set; } = null!;
+}
